ad76aa0f55b400156811dc320e02ef03d4a8553a
Andrew Osborn <osbornandrewj@gmail.com>
2021-09-23 14:51:05 -0700

    commit tree f098a59abf37588a6d82172145273c31adcf3f2e

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution here
+function findMatching(drivers, person){
+    let pass = drivers.filter(name => name == person || name == person.toLowerCase())
+    return pass    
+} 
+
+function fuzzyMatch (driverArray,name){
+    return driverArray.filter(element => element.slice(0,1) === name.slice(0,1))
+}
+
+function matchName(drivers, namai){
+    const dudebros = drivers.filter(person => person.name === namai)
+    return dudebros
+}
